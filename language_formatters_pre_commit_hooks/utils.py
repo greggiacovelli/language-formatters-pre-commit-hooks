f6b82c7eae7b930d613fd20a2fcded0daa60cf3c
Greg Giacovelli <greg.giacovelli@gotinder.com>
2021-04-16 09:49:27 -0700

    time formatting

--- language_formatters_pre_commit_hooks/utils.py
+++ language_formatters_pre_commit_hooks/utils.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import datetime
+from datetime import datetime
 import os
 import shutil
 import subprocess  # nosec: disable=B603
